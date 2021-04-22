cab3a3034219776ee115b0b927ee46c22182fa7a
Your Name <you@example.com>
2021-04-22 22:29:16 +0100

    Inputs and Checkbox Passed Cypress Test

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -6,12 +6,51 @@ describe("Quotes app", () => {
 
      it("sanity test to make sure tests work", () => {
         expect(1 + 2).to.equal(3);
-        expect(2 + 2).not.to.equal(5);
-              
+        expect(2 + 2).not.to.equal(5);     
         });
 
+    const firstNameInput = () => cy.get('input[name="first_name"]');
+    const lastNameInput = () => cy.get('input[name="last_name"]');
+    const emailInput = () => cy.get('input[name="email"]');
+    const passwordInput = () => cy.get('input[name="pwd"]');
+    const checkboxInput = () => cy.get('input[name="tos"]');
+    // const submitBtn = () => cy.get('button[class="submit"]');
+
+    it("can type in the inputs", () => {
+          firstNameInput()
+            .should("have.value", "")
+            .type("Seye")
+            .should("have.value", "Seye");
+
+          lastNameInput()
+            .should("have.value", "")
+            .type("Onigbinde")
+            .should("have.value", "Onigbinde");
+
+         emailInput()
+            .should("have.value", "")
+            .type("seyeonigbinde@gmail.com")
+            .should("have.value", "seyeonigbinde@gmail.com");
+
+         passwordInput()
+            .should("have.value", "")
+            .type("Nigeria")
+            .should("have.value", "Nigeria");
+
+        checkboxInput()
+            .should("not.be.checked")
+            .check()
+            .should("be.checked");
+
+
+        
+        });
 });
   
+
+
+
+
 
 
 
